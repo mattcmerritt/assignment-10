3ca77a2add6c192173f167e568af3fe6d5858f3b
mattcmerritt <mattcmerritt@gmail.com>
2025-05-09 19:28:45 -0400

    websocket updates

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,23 +1,43 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './List.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getEntries } from '../utils/api';
 import SmallEntry from '../components/items/SmallEntry';
 
+const WSS_URL = `ws://localhost:8080`;
+
 export default function List() {
     const navigate = useNavigate();
-
+    const [updateDetected, setUpdateDetected] = useState(true);
     const [items, setItems] = useState([]);
-
-    // load item list on render
-    useEffect(() => {
-        getEntries(localStorage.getItem('TOKEN')).then((entries) => setItems(entries));
-    }, []);
+    const ws = useRef(null as WebSocket); 
 
     // check if not logged in -> go to login screen
     useEffect(() => {
         if (!localStorage.getItem('TOKEN')) {
             navigate('/login');
+        }
+    }, []);
+
+    // load item list on render
+    useEffect(() => {
+        setItems([]); // necessary, if list length stays the same a render will not be issued
+        getEntries(localStorage.getItem('TOKEN')).then((entries) => setItems(entries));
+        setUpdateDetected(false);
+    }, [updateDetected]);
+
+    // create websocket to listen for events
+    useEffect(() => {
+        // create new socket and store in ref
+        const socket = new WebSocket(WSS_URL);
+        ws.current = socket;
+        socket.addEventListener('message', (e) => {
+            const message : { updateRequired : boolean } = JSON.parse(e.data);
+            setUpdateDetected(message.updateRequired);
+        });
+        // cleanup
+        return () => {
+            socket.close();
         }
     }, []);
 
